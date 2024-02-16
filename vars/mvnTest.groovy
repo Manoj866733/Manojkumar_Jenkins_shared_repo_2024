90c0efdd63595ebe1b8453a85e32caa7a4c96f94
Manoj866733 <manojkumar.r658@gmail.com>
2024-02-16 19:54:31 +0530

    updated

--- vars/mvnTest.groovy
+++ vars/mvnTest.groovy
@@ -4,6 +4,9 @@ def call(String directory = '.') {
     // Add the 'directory' parameter to change to the specified directory
     dir(directory) {
         // Run Maven test
-        sh 'mvn -f path/to/maven/project/pom.xml test'
+        script {
+            // Run Maven test
+            sh 'mvn test'
+        }
     }
 }
